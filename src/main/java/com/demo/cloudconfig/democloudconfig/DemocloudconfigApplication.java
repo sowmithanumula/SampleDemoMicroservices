f5da88c2cf574583f6b25357bd551fcc70900786
Anumula Sowmith <sowmi65@gmail.com>
2022-12-11 12:42:17 +0530

    First commit

---
+++ src/main/java/com/demo/cloudconfig/democloudconfig/DemocloudconfigApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cloudconfig.democloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocloudconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocloudconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/cloudconfig/democloudconfig/DemocloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cloudconfig.democloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
